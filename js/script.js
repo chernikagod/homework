436a1e605b3d04a683e15f8eec893fe8816fdb10
danchern <chernikagod2@gmaio.com>
2024-04-25 20:58:00 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+$(document).ready(function () {
+  $("#languages").on("click", "p", function () {
+    $(this).addClass("active").siblings().removeClass("active");
+  });
 
+  $("#menu").on("click", "p", function () {
+    $(this).addClass("active-menu").siblings().removeClass("active-menu");
+  });
+
+  $(".burger").click(function () {
+    $(this).toggleClass("burger-action");
+    $("#nav").toggleClass("burger-action");
+  });
+
+  $("#nav").on("click", "li", function () {
+    $(this).addClass("active-li").siblings().removeClass("active-li");
+  });
+
+  $("#circles").on("click", ".circle", function (event) {
+    var selectedCircle = $(event.target);
+    if (selectedCircle.is("div")) {
+      $("#circles .circle").removeClass("circle-active");
+      selectedCircle.addClass("circle-active");
+    }
+  });
+});
